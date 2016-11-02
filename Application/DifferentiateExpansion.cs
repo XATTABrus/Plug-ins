cd57e15c2480131a072e8363af186795a6afd824
XATTAB <XATTAB.rus@gmail.com>
2016-11-02 20:18:31 +0500

    Добавление второго задания с тестами.

---
+++ Application/DifferentiateExpansion.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Application
+{
+    public static class DifferentiateExpansion
+    {
+        /// <summary>
+        /// Дефиринцирует заданное лямбда выражение.
+        /// </summary>
+        /// <param name="source">Исходное лямбда выражение</param>
+        /// <returns>Expression</returns>
+        public static Expression<Func<double, double>> Differentiate(this Expression<Func<double, double>> source)
+        {
+            return Expression.Lambda(ParsingExpression(source.Body), source.Parameters[0]) as Expression<Func<double, double>>;
+        }
+
+        /// <summary>
+        /// Дефиринцирует заданное лямбда выражение и возвращает скомилированное лямбда выражение.
+        /// </summary>
+        /// <param name="source">Исходное лямбда выражение</param>
+        /// <param name="compile">Компилировать выражение</param>
+        /// <returns>Lambda</returns>
+        public static Func<double, double> DifferentiateAndCompile(this Expression<Func<double, double>> source)
+        {
+            return Expression.Lambda(ParsingExpression(source.Body), source.Parameters[0]).Compile() as Func<double, double>;
+        }
+
+        /// <summary>
+        /// Дифференцирование исходного вырожения. 
+        /// </summary>
+        /// <param name="expression">Исходное выражение</param>
+        /// <returns></returns>
+        private static Expression ParsingExpression(Expression expression)
+        {
+            // Производная от константы = 0
+            if (expression is ConstantExpression) { return Expression.Constant(0.0); }
+
+            // Производная от x = 1
+            if (expression is ParameterExpression) { return Expression.Constant(1.0); }
+
+            // Производная от вырожений типа + и *
+            if (expression is BinaryExpression)
+            {
+                var ex = expression as BinaryExpression;
+
+                switch (ex.NodeType)
+                {
+                    case ExpressionType.Add:
+                        return Expression.Add(ParsingExpression(ex.Left), ParsingExpression(ex.Right));
+
+                    case ExpressionType.Multiply:
+                        return Expression.Add(
+                            Expression.Multiply(ex.Left, ParsingExpression(ex.Right)),
+                            Expression.Multiply(ex.Right, ParsingExpression(ex.Left)));
+
+                    default:
+                        throw new ArgumentException("Поддерживается только сложение и умножение!");
+                }
+            }
+
+            // Производная от Sin(x)
+            if (expression is MethodCallExpression)
+            {
+                var ex = expression as MethodCallExpression;
+                
+                if (ex.Method.Name == "Sin")
+                {
+                    var arg = ex.Arguments[0];
+                    return Expression.Multiply(Expression.Call(typeof(Math).GetMethod("Cos"), arg), ParsingExpression(arg));
+                }
+
+                throw new ArgumentException("Поддерживается только функиця Sin(x)!");
+            }
+
+            throw new ArgumentException("Не верное выражение!");
+        }
+    }
+}
---
+++ DifferentiateTests/DifferentiateTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq.Expressions;
+using Application;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DifferentiateTests
+{
+    [TestClass]
+    public class DifferentiateTest
+    {
+        [TestMethod]
+        public void SimpleTest()
+        {
+            Expression<Func<double, double>> f = x => x * x;
+            var df = f.DifferentiateAndCompile();
+
+            var result = df.Invoke(12);
+
+            Assert.AreEqual(24, result);
+        }
+
+        [TestMethod]
+        public void SinTest()
+        {
+            Expression<Func<double, double>> f = x => (10 + Math.Sin(x)) * x;
+            var df = f.DifferentiateAndCompile();
+            
+            var result = df.Invoke(12);
+
+            Assert.AreEqual(19.5897, Math.Round(result, 4));
+        }
+    }
+}
